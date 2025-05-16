5404f96b8b1d16b46b9165b5b9c33cd8e05a6ccc
Ebert Daniel (XC-AS/EPO1) <daniel.ebert@de.bosch.com>
2025-05-16 14:45:54 +0200

    fix no blacklisted column in db

--- src/main.py
+++ src/main.py
@@ -434,7 +434,8 @@ async def get_metrics(db: Session = Depends(get_db)):
     labeled = db.query(LabelTask).filter(LabelTask.is_labeled == True).filter(not_(filter_backlisted)).filter(not_(filter_fmc_backlisted)).count()
     not_labeled = db.query(LabelTask).filter(LabelTask.is_labeled == False).filter(not_(filter_backlisted)).filter(not_(filter_fmc_backlisted)).count()
     opened = db.query(LabelTask).filter(LabelTask.sent_label_request_at_epoch != 0).filter(not_(filter_backlisted)).filter(not_(filter_fmc_backlisted)).count()
-    blacklisted = db.query(LabelTask).filter(or_(filter_backlisted,filter_fmc_backlisted)).count()
+    # Not implemented in Metric yet. if we want to support it, need to change database Metric table to add blacklisted column
+    # blacklisted = db.query(LabelTask).filter(or_(filter_backlisted,filter_fmc_backlisted)).count()
 
     opened_pending = opened - labeled
 
@@ -444,7 +445,7 @@ async def get_metrics(db: Session = Depends(get_db)):
         'not_labeled': not_labeled - opened_pending,
         'opened': opened,
         'opened_pending': opened_pending,
-        'blacklisted': blacklisted
+        # 'blacklisted': blacklisted
     }
 
     metric = Metric(**metrics, created_at_epoch=int(time.time()))
